d8976e44bbb892c64f87b661adb73dbbee46967b
Soumen Halder <soumen.tifr2016@gmail.com>
2024-07-06 14:36:15 +0530

    Initial commit

---
+++ src/BlockLayout.js
@@ -0,0 +1,16 @@
+// src/BlockLayout.js
+
+import React from 'react';
+import './BlockLayout.css';
+
+const BlockLayout = () => {
+    return (
+        <div className="container">
+            <div className="block block1">Block 1</div>
+            <div className="block block2">Block 2</div>
+            <div className="block block3">Block 3</div>
+        </div>
+    );
+};
+
+export default BlockLayout;
